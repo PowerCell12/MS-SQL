3a357c564a683526404be9a862ec266497835b2c
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-07-14 13:28:26 +0300

    Create 04_Insert_Records_in_Both_Tables.sql

---
+++ Databases Introduction/Lab/04_Insert_Records_in_Both_Tables.sql
@@ -0,0 +1,13 @@
+insert into Towns
+VALUES
+	(1, 'Sofia'),
+    (2, 'Plovdiv'),
+    (3, 'Varna');
+
+insert into Minions
+values
+	(1, 'Kevin', 22, 1),
+    (2, 'Bob', 15, 3),
+    (3, 'Steward', NULL, 2);
+    
+    
